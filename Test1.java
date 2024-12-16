70fcc6ed8523083ea6ab834ad2907283c91818a6
Pankaj0901 <pankaj.jadhav7278@gmail.com>
2024-12-16 13:44:13 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,17 @@
+package automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		// System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chromedriver-win64.exe"); // Replace with your ChromeDriver path
+
+	        // Initialize WebDriver
+	        WebDriver driver = new ChromeDriver();
+
+	}
+
+}
